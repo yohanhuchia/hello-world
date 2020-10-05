53ac8dd863977a34f2a18783d3f2d44ac265be4b
yohanhuchia <72366400+yohanhuchia@users.noreply.github.com>
2020-10-04 22:31:19 -0400

    Add files via upload

---
+++ phishing-inst.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+clear
+pip install lolcat
+apt install toilet -y 
+clear 
+toilet -f big '  Z3R0-HACK'| lolcat
+sleep 0.5
+
+clear
+cowsay -f daemon Z3R0 | lolcat
+toilet -f big "script " |  lolcat
+toilet -f big "Para " |  lolcat
+toilet -f big "instalar " |  lolcat
+toilet -f big "erramientas " |  lolcat
+toilet -f big "phishing " |  lolcat
+sleep 1.0 
+echo " "
+echo " "
+echo " "
+
+echo "escriba el # de la erramienta q vas a instalar" | lolcat
+echo " "
+echo "weeman:1" | lolcat 
+echo "foto-sploit:2" | lolcat
+echo "HiddenEye:3" | lolcat
+echo "scam:4" | lolcat
+echo "BlackEye:5" | lolcat
+echo "AIOphish:6" | lolcat
+echo "shellphish:7" | lolcat
+echo "formphish:8" | lolcat
+echo "Recreator-Phishing:9" | lolcat
+read A 
+if [ $A == 1 ];
+then
+   clear
+   git clone https://github.com/evait-security/weeman
+   clear
+   echo "weeman instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "sin descripción "
+fi  
+
+if [ $A == 2 ];
+then
+   clear
+   git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+   clear
+   echo "FotoSploit instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "FotoSploit es una herramienta que se especializa en la ingenieria social, Ésta herramienta permite al usuario que la utilice, subir una imágen a un enlace phishing, por lo tanto, será mas probable que la víctima acceda al enlace si ve una imágen que le llame la atención. " | lolcat
+fi
+
+if [ $A == 3 ];
+then
+   clear
+   git clone https://github.com/DarkSecDevelopers/HiddenEye
+   clear
+   echo "HiddenEye instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "Como herramienta de phishing moderna, Hidden Eye es muy buena en lo que hace. La combinación perfecta de todos sus componentes funcionales le da una ventaja al atacar cuentas. Mediante el uso de ataques de fuerza bruta, puede acceder efectivamente a la información personal del usuario. Hidden Eye puede descifrar fácilmente las contraseñas de los usuarios y también puede recopilar otros datos personales que pertenecen a la víctima. " | lolcat
+fi
+
+if [ $A == 4 ];
+then
+   clear
+   git clone https://github.com/Cesar-Hack-Gray/scam
+   clear
+   echo "scam instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "sin descripción " | lolcat
+fi
+
+if [ $A == 5 ];
+then
+   clear
+   git clone https://github.com/thelinuxchoice/blackeye 
+   clear
+   echo "blackeye instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "Blackeye es la herramienta de phishing más completa hasta ahora con 37 plantillas web +1 personalizables. Puede recopilar información de IP y ubicación simplemente haciendo clic en el enlace. " | lolcat
+fi
+
+if [ $A == 6 ];
+then
+   clear 
+   git clone https://github.com/theb3stt/AIOPhish
+   clear 
+   echo "AIOPhish instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "AIOPhish es una herramienta de phishing con 40 plantillas preconfiguradas, además, se especializa en la ingenieria social, ya que, nos brinda la opción de añadir una imagen, título y descripción al enlace y de ésta forma aumentar las probabilidades de que nuestra víctima acceda al enlace, puedes elegir el túnel y como extra tiene su respectivo menu de ayuda con un acortador de URL. " | lolcat
+fi
+
+if [ $A == 7 ];
+then
+   clear
+   git clone https://github.com/thelinuxchoice/shellphish
+   clear 
+   echo "shellphish instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "sin descripción " | lolcat
+fi
+
+if [ $A == 8 ];
+then
+   clear
+   git clone https://github.com/thelinuxchoice/formphish
+   clear
+   echo " "
+   echo " "
+   echo " "   
+   echo "Formphish instalado correctamente" | lolcat 
+   echo "Formphish es una herramienta que copia el código html de una página web a tu elección y te brinda un enlace phishing automáticamente del código de la página web" | lolcat
+fi  
+
+if [ $A == 9 ];
+then
+   clear 
+   git clone https://github.com/AngelSecurityTeam/Recreator-Phishing
+   clear 
+   echo "recreator-phishing instalado correctamente" | lolcat
+   echo " "
+   echo " "
+   echo " "   
+   echo "sin descripción " | lolcat
+fi  
+
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "instalacion completada, si desea instalar otro cierre y vuelva a abrir" | lolcat
